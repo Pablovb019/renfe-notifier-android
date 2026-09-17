48d3b9f4167f4c39c213a0b7d7110ea77f2981d2
Pablo Vilar Bustillo <pavilbus019@gmail.com>
2026-09-17 22:38:20 +0200

    chore: bump version to 0.1.1 (code 2)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         minSdk = 26
         // Android 13 y superiores; nunca un targetSdk antiguo para eludir restricciones.
         targetSdk = 34
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
